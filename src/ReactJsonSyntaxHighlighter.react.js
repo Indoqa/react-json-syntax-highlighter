0bb1c9a522dc2c5efc344b262b3b2ad0e081a70d
Reinhard Pötz <reinhard.poetz@indoqa.com>
2016-04-29 20:25:23 +0200

    introduce react component

--- src/ReactJsonSyntaxHighlighter.react.js
+++ src/ReactJsonSyntaxHighlighter.react.js
@@ -1,5 +1,41 @@
-export default function ReactJsonSyntaxHighlighter() {
+import React, {PropTypes} from 'react'
+
+import './ReactJsonSyntaxHighlighter.styl'
+
+// see // see http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+const ReactJsonSyntaxHighlighter = ({obj}) => {
+  if (Object.keys(obj).length === 0) {
+    return null
+  }
+
+  let json = JSON.stringify(obj, undefined, 2)
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    (match) => {
+      let cls = 'number'
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'key'
+        } else {
+          cls = 'string'
+        }
+      } else if (/true|false/.test(match)) {
+        cls = 'boolean'
+      } else if (/null/.test(match)) {
+        cls = 'null'
+      }
+      return `<span class='${cls}'>${match}</span>`
+    })
+
   return (
-    <div>ReactJsonSyntaxHighlighter</div>
+    <div className="ReactJsonSyntaxHighlighter">
+      <pre dangerouslySetInnerHTML={{__html: json}} />
+    </div>
   )
 }
+
+ReactJsonSyntaxHighlighter.propTypes = {
+  obj: PropTypes.object.isRequired,
+}
+
+export default ReactJsonSyntaxHighlighter
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/index',
@@ -7,10 +8,14 @@ module.exports = {
       {
         test: /\.js?/,
         excludes: /(node_modules)/,
-        loader: "babel",
+        loader: 'babel',
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!stylus-loader')
       }
     ]
   },
@@ -28,12 +33,16 @@ module.exports = {
     library: 'ReactJsonSyntaxHighlighter'
   },
   plugins: [
+    new ExtractTextPlugin('dist/ReactJsonSyntaxHighlighter.css', {
+      allChunks: true
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        screw_ie8: true
+        screw_ie8: true,
+        warnings: false
       }
     })
   ]
-};
+}
